3b1503814be9cb72fac4dad889d0a39dbe68e3ac
Silvio Sabo <44360609+sillen102@users.noreply.github.com>
2025-01-29 20:05:01 +0100

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -61,7 +61,7 @@ fi
 
 if [ -n "${INPUT_PRIVATE_REGISTRY}" ] && [ -n "${INPUT_PRIVATE_REGISTRY_USER}" ] && [ -n "${INPUT_PRIVATE_REGISTRY_TOKEN}" ]; then
     echo -e "\u001b[36mLogging in to private registry"
-    docker login --username "${INPUT_PRIVATE_REGISTRY_USER}" "${INPUT_PRIVATE_REGISTRY}" "${INPUT_PRIVATE_REGISTRY_TOKEN}"
+    echo "${INPUT_PRIVATE_REGISTRY_TOKEN}" | docker login --username "${INPUT_PRIVATE_REGISTRY_USER}" "${INPUT_PRIVATE_REGISTRY}" --password-stdin
     echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME} \u001b[36mwith Registry Auth."
     docker stack deploy -c "${INPUT_FILE}" "${INPUT_NAME}" --with-registry-auth
 else
